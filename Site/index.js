7161c737a08e33c5d0b485d14eb5cc458001cf06
Sir Code <taylorschneider07@yahoo.com>
2024-04-25 23:39:33 -0400

    Tests

--- Site/index.js
+++ Site/index.js
@@ -14,20 +14,58 @@ function start() {
     ctx.fillRect(0, 0, cvs.width, cvs.height);
     ctx.fillStyle = "white";
     let fxd = [];
-    mnd.forEach((min) => {
+    mnd.forEach((min, i) => {
         if ((min.vhnmin > 0 || min.vhnmax > 0) && (min.hmin > 0 || min.hmax > 0)) {
-            let moh = (min.hmin + min.hmax) * 0.5;
-            let vik = ((+min.vhnmin) + (+min.vhnmax)) * 0.5;
+            let moh = 0;
+            if (min.hmax > 0 && min.hmin > 0) {
+                moh = (min.hmin + min.hmax) / 2;
+            } else { 
+                moh = min.hmin || min.hmax; 
+            }
+            let vik = 0;
+            if (+min.vhnmax > 0 && +min.vhnmin > 0) {
+                vik = ((+min.vhnmin) + (+min.vhnmax)) / 2;
+            } else {
+                vik = +min.vhnmin || +min.vhnmax;
+            }
             let newmin = min;
             newmin.mohs = moh;
             newmin.viks = vik;
             fxd.push(newmin);
         }
     });
+    //console.log(fxd);
     const ref = (cvs.width - 2 * marg) / fxd.length;
     fxd.forEach((min, i) => {
-        let x = map(min.mohs, 0, 10, 0, (cvs.width));
-        let y = map(min.viks, 0, 5000, (cvs.height - marg), marg);
+
+        // Horizontal Vickers / Vertical Mohs
+        // let x1 = map(+min.vhnmin || min.viks, 0, 3000, marg, cvs.width - marg);
+        // let x2 = map(+min.vhnmax || min.viks, 0, 3000, marg, cvs.width - marg);
+        // let x3 = map(min.viks, 0, 3000, marg, cvs.width - marg);
+        // let y1 = map(min.hmin || min.mohs, 0, 10, cvs.height, marg);
+        // let y2 = map(min.hmax || min.mohs, 0, 10, cvs.height, marg);
+        // let y3 = map(min.mohs, 0, 10, cvs.height, marg);
+
+        // Horizontal Mohs / Vertical Vickers
+        // let x1 = map(min.hmin || min.mohs, 0, 10,  0, cvs.width - marg);
+        // let x2 = map(min.hmax || min.mohs, 0, 10,  0, cvs.width - marg);
+        // let x3 = map(min.mohs, 0, 10,  0, cvs.width - marg);
+        // let y1 = map(+min.vhnmin || min.viks, 0, 5000, cvs.height - marg, marg);
+        // let y2 = map(+min.vhnmax || min.viks, 0, 5000, cvs.height - marg, marg);
+        // let y3 = map(min.viks, 0, 5000, cvs.height - marg, marg);
+
+        let x = map(min.viks, 0, 3000,  marg, cvs.width - marg);
+        let y = map(+min.gravrep, 0, 50, cvs.height - marg, marg);
+
+        // Three Color
+        // ctx.fillStyle = "#ff0000";
+        // ctx.fillRect(x1, y1, 1, 1);
+        // ctx.fillStyle = "#ffff00";
+        // ctx.fillRect(x2, y2, 1, 1);
+        // ctx.fillStyle = "#00ffff";
+        // ctx.fillRect(x3, y3, 1, 1);
+
+        // Single Color
         ctx.fillRect(x, y, 1, 1);
     });
 }
