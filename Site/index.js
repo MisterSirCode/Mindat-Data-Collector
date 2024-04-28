cc8d6195fe73b37850b6d6f01231f3a55c12e673
Sir Code <taylorschneider07@yahoo.com>
2024-04-28 15:58:03 -0400

    Fixed up some stuff

--- Site/index.js
+++ Site/index.js
@@ -9,49 +9,65 @@ function map(value, in_min, in_max, out_min, out_max) {
     return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+function vickersVsDensity(ctx, marg) {
+    ctx.fillText("Vickers Hardness (x) compared to Density (y)", 30, 50);
+    mnd.forEach((min, i) => {
+        if (min.viks > 0 && min.dens > 0) {
+            let x = map(min.viks, 0, 3000, marg, cvs.width - marg * 2);
+            let y = map(min.dens, 3, 30, cvs.height - marg * 2, marg);
+            ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+            ctx.fillRect(x, y, 2, 2);
+        }
+    });
+}
+
+function vickersVsMohs(ctx, marg) {
+    ctx.fillText("Vickers Hardness (x) compared to Mohs Hardness (y)", 30, 50);
+    mnd.forEach((min, i) => {
+        if (min.viks > 0 && min.mohs > 0) {
+            let x = map(min.viks, 0, 3000, marg, cvs.width - marg * 2);
+            let y = map(min.mohs, 1.5, 12, cvs.height - marg * 2, marg);
+            ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+            ctx.fillRect(x, y, 2, 2);
+        }
+    });
+}
+
+function densityVsIor(ctx, marg) {
+    ctx.fillText("Density (x) compared to Refractive Index (y)", 30, 50);
+    mnd.forEach((min, i) => {
+        if (min.dens > 0 && min.ior > 0) {
+            let x = map(min.dens, 1, 10, marg, cvs.width - marg * 2);
+            let y = map(min.ior, 1.4, 4, cvs.height - marg * 2, marg);
+            ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+            ctx.fillRect(x, y, 2, 2);
+        }
+    });
+}
+
+function densityVsMohs(ctx, marg) {
+    ctx.fillText("Density (x) compared to Mohs Hardness (y)", 30, 50);
+    mnd.forEach((min, i) => {
+        if (min.dens > 0 && min.mohs > 0) {
+            let x = map(min.dens, 0.5, 25, marg, cvs.width - marg * 2);
+            let y = map(min.mohs, 1.2, 12, cvs.height - marg * 2, marg);
+            ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+            ctx.fillRect(x, y, 2, 2);
+        }
+    });
+}
+
 function start() {
     cvs.width = cvs.offsetWidth;
     cvs.height = cvs.offsetHeight;
-    const marg = cvs.height / 20;
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
     ctx.fillStyle = "white";
+    const marg = cvs.height / 20;
     const ref = (cvs.width - 2 * marg) / mnd.length;
-    mnd.forEach((min, i) => {
-        if (i == 0)
-            densest = min;
-        else if (densest.dens < min.dens)
-            densest = min;
-        // Horizontal Vickers / Vertical Mohs
-        // let x1 = map(+min.vhnmin || min.viks, 0, 3000, marg, cvs.width - marg);
-        // let x2 = map(+min.vhnmax || min.viks, 0, 3000, marg, cvs.width - marg);
-        // let x3 = map(min.viks, 0, 3000, marg, cvs.width - marg);
-        // let y1 = map(min.hmin || min.mohs, 0, 10, cvs.height, marg);
-        // let y2 = map(min.hmax || min.mohs, 0, 10, cvs.height, marg);
-        // let y3 = map(min.mohs, 0, 10, cvs.height, marg);
-
-        // Horizontal Mohs / Vertical Vickers
-        // let x1 = map(min.hmin || min.mohs, 0, 10,  0, cvs.width - marg);
-        // let x2 = map(min.hmax || min.mohs, 0, 10,  0, cvs.width - marg);
-        // let x3 = map(min.mohs, 0, 10,  0, cvs.width - marg);
-        // let y1 = map(+min.vhnmin || min.viks, 0, 5000, cvs.height - marg, marg);
-        // let y2 = map(+min.vhnmax || min.viks, 0, 5000, cvs.height - marg, marg);
-        // let y3 = map(min.viks, 0, 5000, cvs.height - marg, marg);
-
-        let x = map(min.viks, 0, 3000,  marg, cvs.width - marg);
-        let y = map(+min.gravrep, 0, 50, cvs.height - marg, marg);
-
-        // Three Color
-        // ctx.fillStyle = "#ff0000";
-        // ctx.fillRect(x1, y1, 1, 1);
-        // ctx.fillStyle = "#ffff00";
-        // ctx.fillRect(x2, y2, 1, 1);
-        // ctx.fillStyle = "#00ffff";
-        // ctx.fillRect(x3, y3, 1, 1);
-
-        // Single Color
-        ctx.fillRect(x, y, 1, 1);
-    });
+    ctx.fillStyle = "white";
+    ctx.font = "20px sans-serif";
+    densityVsMohs(ctx, marg);
 }
 
 fetch('database.json').then((res) => {
